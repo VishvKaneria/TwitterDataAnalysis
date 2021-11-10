d1d018951208f398d33dc7c5d0280d242f315cb9
Vishv Kaneria <72430543+VishvKaneria@users.noreply.github.com>
2021-11-10 19:54:47 +0530

    Add files via upload

---
+++ TwitterAnalysis.py
@@ -0,0 +1,81 @@
+from textblob import TextBlob
+import sys
+import tweepy
+from tweepy import API
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+
+consumer_key = "aYECxzoXDMGFvcNz39iIkmU8a"
+consumer_secret = "T5SKz3ZlrjZxbiPdzSEuDa4ijeEbWvFfIIvpbaXuQWrOtMxZm7"
+access_token = "3149444029-lXsEDHbLv9GrTvjMha3KO6VcUoWTxUdMGWyVZoo"
+access_token_secret = "9xKSQCTCxzbniqEKW41X5Tw2Zwz4HZR2PHHxqLmZbGGSp"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+def percentage(part, whole):
+  return 100 * float(part)/float(whole)
+  
+keyword = input("Please enter keyword or hashtag to search: ")
+noOfTweet = int(input("Please enter number of tweets: "))
+tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(noOfTweet)
+positive = 0
+negative = 0
+neutral = 0
+polarity = 0
+tweet_list = []
+neutral_list = []
+negative_list = []
+positive_list = []
+for tweet in tweets:
+  
+  #distribution
+  tweet_list.append(tweet.text)
+  analysis = TextBlob(tweet.text)
+  score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
+  neg = score['neg']
+  neu = score['neu']
+  pos = score['pos']
+  comp = score['compound']
+  polarity += analysis.sentiment.polarity
+  
+  if neg > pos:
+      negative_list.append(tweet.text)
+      negative += 1
+  elif pos > neg:
+      positive_list.append(tweet.text)
+      positive += 1
+  
+  elif neg == pos:
+      neutral_list.append(tweet.text)
+      neutral += 1
+  
+positive = percentage(positive, noOfTweet)
+negative = percentage(negative, noOfTweet)
+neutral = percentage(neutral, noOfTweet)
+polarity = percentage(polarity, noOfTweet)
+positive = format(positive, '.1f')
+negative = format(negative, '.1f')
+neutral = format(neutral, '.1f')
+labels = ['Positive ['+str(positive)+'%]' , 'Neutral ['+str(neutral)+'%]', 'Negative ['+str(negative)+'%]']
+sizes = [positive, neutral, negative]
+colors = ['green', 'yellow', 'red']
+patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+plt.style.use('default')
+plt.legend(labels)
+plt.title("Keyword = " +keyword+ "")
+plt.axis('equal')
+plt.show()
